6fd74003b440849aba783b9dcec5dcc919c7a40a
Harriettt-dev <ant.harriettt@gmail.com>
2026-07-25 14:05:36 +0000

    Add prime checking function to the program

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,4 +36,21 @@
 
 #include <iostream>
 using namespace std;
+bool isprime(int number)
+ { if (number < 2) 
+   { return false; }
+    for (int i = 2; i < number; i++)
+      { if (number % i ==0)
+        { return false; } }
+        return true; }
+   int main()
+    { int number;
+      cout << "Enter a number: ";
+      cin >> number;
+      if (isprime(number))
+    { cout << number << " is a prime number."; }
+   else
+   { cout << number << " is Not a prime number."; }
+    return 0;
+    }
 
