978f3c3415e35dc165bb69f4eae062968e5949f6
Harriettt-dev <ant.harriettt@gmail.com>
2026-07-26 19:37:58 +0000

    Add functions for sum, average, max, and min

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,69 @@
 #include <iostream>
 using namespace std;
 
+
+// Function to calculate sum
+int findSum(int arr[], int n)
+
+{
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    { sum = sum + arr[i]; }
+    return sum;
+}
+
+// Function to calculate average
+double findAverage(int arr[], int n)
+{ int sum = findSum(arr, n);
+   return (double)sum / n;
+}
+
+// Function to find maximum value
+int findMaximum(int arr[], int n)
+{ int max = arr[0];
+
+   for(int i = 1; i < n; i++)
+    {
+      if(arr[i] > max)
+        { max = arr[i]; }
+    }
+
+    return max;
+}
+
+// Function to find minimum value
+int findMinimum(int arr[], int n)
+{ int min = arr[0];
+
+    for(int i = 1; i < n; i++)
+    { if(arr[i] < min)
+        { min = arr[i]; }
+    }
+
+    return min;
+}
+
+int main()
+{ int n;
+ cout << "How many numbers? ";
+    cin >> n;
+ if(n <= 0)
+    { cout << "Error: Number of values must be positive." << endl;
+        return 0;
+    }
+ int arr[n];
+
+    for(int i = 0; i < n; i++)
+    { cout << "Enter number " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "\nResults:" << endl;
+    cout << "Sum: " << findSum(arr, n) << endl;
+    cout << "Average: " << findAverage(arr, n) << endl;
+    cout << "Maximum: " << findMaximum(arr, n) << endl;
+    cout << "Minimum: " << findMinimum(arr, n) << endl;
+
+    return 0;
+}
+
