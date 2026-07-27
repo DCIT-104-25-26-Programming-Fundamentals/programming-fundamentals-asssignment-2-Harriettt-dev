3f96041d50a954a8d5372a56d5aa13bdd5d8bd34
Harriettt-dev <ant.harriettt@gmail.com>
2026-07-27 18:56:56 +0000

    Add multiplication table functions for single and all tables

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,51 @@
 #include <iostream>
 using namespace std;
 
+
+
+// Part A
+null singleTable(int num)
+{
+    cout << "\nMultiplication Table for " << num << ":" << endl;
+
+    for(int i = 1; i <= 12; i++)
+    {  cout << num << " x " << i << " = " << num * i << endl; }
+}
+
+// Part B
+null allTables(int n)
+{
+    for(int i = 1; i <= n; i++)
+    { cout << "\nMultiplication Table for " << i << ":" << endl;
+  for(int j = 1; j <= 12; j++)
+        {cout << i << " x " << j << " = " << i * j << endl; }
+
+        cout << "------------------------" << endl;
+    }
+}
+
+int main()
+{
+    int num, n;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(num <= 0)
+    { cout << "Error: Enter a positive number." << endl;
+        return 0; }
+
+    singleTable(num);
+
+    cout << "\nEnter a number N: ";
+    cin >> n;
+
+    if(n <= 0)
+    { cout << "Error: Enter a positive number." << endl;
+        return 0; }
+
+    allTables(n);
+
+    return 0;
+}
+
